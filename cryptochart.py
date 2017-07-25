75fc3321016510f56897827da572d5bd4024db1e
DurandA <arnaud.durand@live.com>
2017-07-25 09:29:47 +0200

    Add cryptochart module

--- cryptochart.py
+++ cryptochart.py
@@ -8,8 +8,10 @@ try:
     from mpl_finance import candlestick_ohlc
 except ImportError:
     from matplotlib.finance import candlestick_ohlc
-from PIL import Image
+from PIL import Image, ImageFont
 import requests
+
+from cryptochart.providers import quotes_historical_kraken_ohlc
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--output", help="save plot as png")
@@ -51,6 +53,15 @@ candlestick_ohlc(ax, quotes, width=1)
 ax.xaxis_date()
 ax.autoscale_view()
 
+ymin, ymax = ax.get_ylim()
+last_low = quotes[-1][3]
+last_high = quotes[-1][2]
+
+ypos = 12 if ymax - last_high > last_low - ymin else h
+
+font = ImageFont.truetype('04B_03__.TTF', 4)
+
+print(ypos)
 if args.output:
     fig.savefig(args.output, dpi=dpi, cmap="bwr", interpolation="none", origin="lower", pad_inches=0)#bbox_inches='tight')
 else:
@@ -60,5 +71,7 @@ else:
         f.seek(0)
         i = Image.open(f)
         inkyphat.set_image(i)
+        inkyphat.text((170, ypos), quotes[-1][4], inkyphat.RED, font)
+        inkyphat.text((180, ypos), 'XETHZUSD', inkyphat.BLACK, font)
         inkyphat.show()
 #plt.show()
