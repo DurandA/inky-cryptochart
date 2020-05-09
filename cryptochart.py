bbc58abd4c061e664135ccd2afe938d34c53e088
userhas404d <29389186+userhas404d@users.noreply.github.com>
2020-05-09 13:05:34 -0400

    Applies black formatting

--- cryptochart.py
+++ cryptochart.py
@@ -3,16 +3,18 @@ from cryptochart import ax, fig, w, h, dpi
 from cryptochart.providers import quotes_historical_kraken_ohlc
 import argparse, io
 from datetime import datetime, timedelta
+
 try:
     from mpl_finance import candlestick_ohlc
 except ImportError:
     from matplotlib.finance import candlestick_ohlc
 from PIL import Image, ImageDraw, ImageFont
 
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--pair", default='XETHZUSD', help="currency pair")
-parser.add_argument('--flip', dest='flip', action='store_true', help="rotate the display")
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--pair", default="XETHZUSD", help="currency pair")
+parser.add_argument(
+    "--flip", dest="flip", action="store_true", help="rotate the display"
+)
 parser.set_defaults(flip=False)
 parser.add_argument("--output", help="save plot as png")
 args = parser.parse_args()
@@ -34,26 +36,29 @@ last_low = quotes[-1][3]
 last_high = quotes[-1][2]
 last_close = quotes[-1][4]
 
-font = ImageFont.truetype('04B_03__.TTF', 8)
+font = ImageFont.truetype("04B_03__.TTF", 8)
 
 with io.BytesIO() as f:
-    fig.savefig(f, dpi=dpi, cmap="bwr", interpolation="none", origin="lower", pad_inches=0)#bbox_inches='tight')
+    fig.savefig(
+        f, dpi=dpi, cmap="bwr", interpolation="none", origin="lower", pad_inches=0
+    )  # bbox_inches='tight')
     f.seek(0)
-    i = Image.open(f)#.convert('P', palette=(0,1,2))
+    i = Image.open(f)  # .convert('P', palette=(0,1,2))
     d = ImageDraw.Draw(i)
     ypos = 0 if ymax - last_high > last_low - ymin else h - 6
 
     if not args.output:
         from inkyphat import RED, BLACK, text, set_image, set_rotation, show
+
         set_image(i)
         if args.flip:
             set_rotation(180)
     else:
-        RED = (255,0,0)
-        BLACK = (0,0,0)
+        RED = (255, 0, 0)
+        BLACK = (0, 0, 0)
         text = d.text
 
-    text((148, ypos), '{:.2f}'.format(last_close), BLACK, font)
+    text((148, ypos), "{:.2f}".format(last_close), BLACK, font)
     text((176, ypos), args.pair, RED, font)
 
     if args.output:
--- cryptochart/providers.py
+++ cryptochart/providers.py
@@ -1,11 +1,15 @@
 import requests
 
+
 def quotes_historical_kraken_ohlc(pair, since, interval=15):
     unix_time = since.strftime("%s")
-    payload = {'pair': pair, 'since': unix_time, 'interval': interval}
-    r = requests.get('https://api.kraken.com/0/public/OHLC', params=payload)
+    payload = {"pair": pair, "since": unix_time, "interval": interval}
+    r = requests.get("https://api.kraken.com/0/public/OHLC", params=payload)
 
     def parse_ohlc(data):
-        return [[int(l[0]), float(l[1]), float(l[2]), float(l[3]), float(l[4])] for l in data]
+        return [
+            [int(l[0]), float(l[1]), float(l[2]), float(l[3]), float(l[4])]
+            for l in data
+        ]
 
-    return parse_ohlc(r.json()['result'][pair])
+    return parse_ohlc(r.json()["result"][pair])
