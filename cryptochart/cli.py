905df164081cab0850e3368bf1aeb432acef6f05
Arnaud Durand <arnaud.durand@live.com>
2022-05-10 21:44:29 +0200

    Package version 0.2

--- cryptochart/cli.py
+++ cryptochart/cli.py
@@ -12,9 +12,10 @@ from PIL import Image, ImageDraw, ImageFont
 from . import fonts, pkg_resources
 
 
-def main(driver='inky'):
+def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--driver', default=None, choices=['inky', 'inkyphat', 'waveshare_epd'])
     parser.add_argument("--pair", default='XETHZUSD', help="currency pair")
     parser.add_argument('--flip', dest='flip', action='store_true', help="rotate the display")
     parser.set_defaults(flip=False)
@@ -44,6 +45,42 @@ def main(driver='inky'):
     RED = (255,0,0)
     BLACK = (0,0,0)
 
+    display = None
+    driver = args.driver
+
+    if driver == 'inky':
+        from inky import InkyPHAT
+        RED = InkyPHAT.RED
+        BLACK = InkyPHAT.BLACK
+
+        display = InkyPHAT('black')
+    elif driver == 'inkyphat':
+        from inkyphat import RED as inky_RED, BLACK as inky_BLACK, set_image as _set_image, show as _show
+        RED = inky_RED
+        BLACK = inky_BLACK
+
+        class InkyPHAT():
+            def __init__(*args):
+                pass
+            set_image = staticmethod(_set_image)
+            show = staticmethod(_show)
+
+        display = InkyPHAT()
+    elif driver == 'waveshare_epd':
+        from waveshare_epd import epd2in13_V2
+
+        class EPD():
+            def __init__(self, *args):
+                self.epd = epd2in13_V2.EPD()
+                self.epd.init(self.epd.FULL_UPDATE)
+                self.buf = []
+            def set_image(self, image):
+                self.buf = self.epd.getbuffer(image)
+            def show(self):
+                self.epd.display(self.buf)
+
+        display = EPD()
+
     with io.BytesIO() as f:
         fig.savefig(f, dpi=dpi, cmap="bwr", interpolation="none", origin="lower", pad_inches=0)
         f.seek(0)
@@ -51,53 +88,19 @@ def main(driver='inky'):
         d = ImageDraw.Draw(i)
         ypos = 0 if ymax - last_high > last_low - ymin else h - 6
 
-        if driver:
-            if driver == 'inky':
-                from inky import InkyPHAT
-                RED = InkyPHAT.RED
-                BLACK = InkyPHAT.BLACK
-
-                display = InkyPHAT('black')
-            if driver == 'inkyphat':
-                from inkyphat import RED as inky_RED, BLACK as inky_BLACK, set_image as _set_image, show as _show
-                RED = inky_RED
-                BLACK = inky_BLACK
-
-                class InkyPHAT():
-                    def __init__(*args):
-                        pass
-                    set_image = staticmethod(_set_image)
-                    show = staticmethod(_show)
-
-                display = InkyPHAT()
-            if driver == 'waveshare_epd':
-                from waveshare_epd import epd2in13_V2
-
-                class EPD():
-                    def __init__(self, *args):
-                        self.epd = epd2in13_V2.EPD()
-                        self.epd.init(self.epd.FULL_UPDATE)
-                        self.buf = []
-                    def set_image(self, image):
-                        self.buf = self.epd.getbuffer(image)
-                    def show(self):
-                        self.epd.display(self.buf)
-
-                display = EPD()
-
-            display.set_image(i.convert("P"))
-
-            if args.flip:
-                i = i.transpose(Image.ROTATE_180)
-
         d.text((148, ypos), '{:.2f}'.format(last_close), BLACK, font)
         d.text((176, ypos), args.pair, RED, font)
 
+        if args.flip:
+            i = i.transpose(Image.ROTATE_180)
+
+        if display:
+            display.set_image(i.convert("P"))
+            display.show()
+
         if args.output:
             i.save(args.output)
-            return
 
-        display.show()
 
 if __name__ == "__main__":
     main()
