ff6b4af84c4b39f42570add0ce2659d9f7b435f5
Arnaud Durand <arnaud.durand@live.com>
2022-05-10 21:44:29 +0200

    Add waveshare_epd driver

--- cryptochart/cli.py
+++ cryptochart/cli.py
@@ -12,7 +12,7 @@ from PIL import Image, ImageDraw, ImageFont
 from . import fonts, pkg_resources
 
 
-def main(use_inky=True):
+def main(driver='inky'):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--pair", default='XETHZUSD', help="currency pair")
@@ -41,6 +41,9 @@ def main(use_inky=True):
     ttf = pkg_resources.open_binary(fonts, '04B_03__.TTF')
     font = ImageFont.truetype(ttf, 8)
 
+    RED = (255,0,0)
+    BLACK = (0,0,0)
+
     with io.BytesIO() as f:
         fig.savefig(f, dpi=dpi, cmap="bwr", interpolation="none", origin="lower", pad_inches=0)
         f.seek(0)
@@ -48,25 +51,44 @@ def main(use_inky=True):
         d = ImageDraw.Draw(i)
         ypos = 0 if ymax - last_high > last_low - ymin else h - 6
 
-        if not args.output:
-            if not use_inky:
-                from inkyphat import RED, BLACK, set_image as _set_image, show as _show
+        if driver:
+            if driver == 'inky':
+                from inky import InkyPHAT
+                RED = InkyPHAT.RED
+                BLACK = InkyPHAT.BLACK
+
+                display = InkyPHAT('black')
+            if driver == 'inkyphat':
+                from inkyphat import RED as inky_RED, BLACK as inky_BLACK, set_image as _set_image, show as _show
+                RED = inky_RED
+                BLACK = inky_BLACK
+
                 class InkyPHAT():
                     def __init__(*args):
                         pass
                     set_image = staticmethod(_set_image)
                     show = staticmethod(_show)
-            else:
-                from inky import InkyPHAT
 
-            display = InkyPHAT()
-            display.set_image(i)
+                display = InkyPHAT()
+            if driver == 'waveshare_epd':
+                from waveshare_epd import epd2in13_V2
+
+                class EPD():
+                    def __init__(self, *args):
+                        self.epd = epd2in13_V2.EPD()
+                        self.epd.init(self.epd.FULL_UPDATE)
+                        self.buf = []
+                    def set_image(self, image):
+                        self.buf = self.epd.getbuffer(image)
+                    def show(self):
+                        self.epd.display(self.buf)
+
+                display = EPD()
+
+            display.set_image(i.convert("P"))
 
             if args.flip:
                 i = i.transpose(Image.ROTATE_180)
-        else:
-            RED = (255,0,0)
-            BLACK = (0,0,0)
 
         d.text((148, ypos), '{:.2f}'.format(last_close), BLACK, font)
         d.text((176, ypos), args.pair, RED, font)
@@ -76,3 +98,6 @@ def main(use_inky=True):
             return
 
         display.show()
+
+if __name__ == "__main__":
+    main()
